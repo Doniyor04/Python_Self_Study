4f2339e8154ab4d256534d038a64259e45ceef07
Doniyor_9393 <96741565+Doniyor04@users.noreply.github.com>
2025-10-15 23:09:20 +0500

    Update management_system_test.py

--- OOP/management_system_test.py
+++ OOP/management_system_test.py
@@ -6,6 +6,7 @@ from management_system import Talaba, Guruh, Fakultet
 talaba1 = Talaba("doniyor", 21.1)
 talaba2 = Talaba("alisher", 19, 2)
 talaba3 = Talaba("otabek", 35, 3)
+talaba4 = Talaba("abdulaziz", 28)
 
 guruh1 = Guruh("19-23-S")
 guruh2 = Guruh("3-25-A")
@@ -16,6 +17,7 @@ fakultet2 = Fakultet("IT")
 
 
 class TalabaTest(unittest.TestCase):
+
     def test_create(self):
         
         self.assertIsNotNone(talaba1.ism)
@@ -35,6 +37,7 @@ class TalabaTest(unittest.TestCase):
         except ValueError as error:
             self.assertEqual(type(error), ValueError)
 
+
     def test_update_bosqich(self):
         talaba1.bosqich = talaba1.update_bosqich()
         self.assertEqual(2, talaba1.bosqich)
@@ -49,6 +52,7 @@ class TalabaTest(unittest.TestCase):
         self.assertEqual(f"{talaba3} {talaba3_bosqich}-kursda. Siz {talaba3} kursini maksimum {5-talaba3_bosqich} ta oshira olasiz", talaba3.bosqich)
 
 class GuruhTest(unittest.TestCase):
+
     def test_create(self):
 
         self.assertIsNotNone(guruh1.nomi)
@@ -57,16 +61,74 @@ class GuruhTest(unittest.TestCase):
         self.assertIsNotNone(guruh1.talabalar)
         self.assertEqual([], guruh1.talabalar)
 
+
     def test_talaba_qoshish(self):
         guruh1.talaba_qoshish(talaba1, talaba2, talaba1)
         self.assertEqual([talaba1, talaba2], guruh1.talabalar)
+
+
+    def test_talaba_ochirish(self):
+        guruh2.talaba_qoshish(talaba3, talaba4)
+
+        id_talaba3 = talaba3.get_idraqam()
+        guruh2.talaba_ochirish(id_talaba3)
+        self.assertEqual([talaba4], guruh2.talabalar)
+
+        guruh2.talaba_ochirish(talaba4.get_idraqam())
+        self.assertEqual([], guruh2.talabalar)
+
 
     def test_get_talabalar(self):
         self.assertEqual([], guruh1.get_talabalar())
         guruh1.talaba_qoshish(talaba1, talaba2, talaba1)
         self.assertEqual(['doniyor', 'alisher'], guruh1.get_talabalar())
 
+
+    def test_oralama_yosh(self):
+
+        oralama_yosh_g3 = guruh3.get_ortalama_yosh()
+        self.assertEqual(f"{guruh3} guruhida talabalar yo‘q.", oralama_yosh_g3)
+
+        guruh2.talaba_qoshish(talaba3, talaba4)
+        oralama_yosh_g2 = guruh2.get_ortalama_yosh()
+        yoshlar = [t.get_yosh() for t in guruh2.talabalar]
+        self.assertEqual(f"{guruh2} guruh o‘rtacha yoshi: {sum(yoshlar)//len(yoshlar)}", oralama_yosh_g2)
+
+
+    def test_show_students(self):
+
+        self.assertEqual(f"{guruh3} guruhida talabalar yo'q", guruh3.show_students())
+
+        list_info =  [f"{talaba}, {talaba.get_bosqich()}-kurs, ID: {str(talaba.get_idraqam())}, Guruh: {talaba.guruh}" for talaba in guruh1.talabalar]
+        self.assertEqual(list_info, guruh1.show_students())
+
+class FakultetTest(unittest.TestCase):
+
+    def test_create(self):
+
+        self.assertIsNotNone(fakultet1.nomi)
+        self.assertEqual("Iqtisod", fakultet1.nomi)
+
+        self.assertIsNotNone(fakultet1.guruhlar)
+
+
+    def test_guruh_qoshish_ochirish(self):
+
+        fakultet1.guruh_qoshish(guruh1, guruh2, guruh1)
+        self.assertEqual([guruh1, guruh2], fakultet1.guruhlar)
+
+        fakultet1.guruh_ochirish(guruh1, guruh2)
+        self.assertEqual([], fakultet1.guruhlar)
+        
+
+    def test_guruh_topish(self):
+        self.assertEqual(None, fakultet1.guruh_topish(guruh1))
+
+        fakultet1.guruh_qoshish(guruh1)
+        self.assertEqual(guruh1, fakultet1.guruh_topish(guruh1.nomi))
+        fakultet1.guruh_ochirish(guruh1)
     
+
 
 if __name__ == '__main__':
     unittest.main()
